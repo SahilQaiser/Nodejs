75e60d57b8d9d9a7ae25dc3edbcd31a26b2686cd
Eram Fatima <eram.fatima@tothenew.com>
2020-06-19 18:51:08 +0530

    after dashboard

--- routes/profile.js
+++ routes/profile.js
@@ -1,32 +1,26 @@
 var express = require('express');
 var router = express.Router();
-// var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/friends', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection to MongoDB failed:'));
-// db.once('open', function() {
-//     console.log("Connected to MongoDB");
-// });
-
-// var schema = new mongoose.Schema({ name: 'string', age: 'number', favColor: 'string' });
-// var Friends = mongoose.model('Friends', schema);
-// Friends.create({ name: 'Sahil', age: 99, favColor: 'BLACK' }, function(err) {
-//     if (err) return handleError(err);
-//     console.log("Friends Collection Created");
-// });
-var name, age, favColor;
-//Ftech docs
-// Friends.find({}, (err, friends) => {
-//     if (err) return handleError(err);
-//     name = friends[0].name;
-//     age = friends[0].age;
-//     favColor = friends[0].favColor;
-// });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('profile', { name: "name", email: "sq@sq.com", phone: "789" });
+    res.render('profile', { layout: 'dashboard-layout' });
+});
+/* GET User Profile. */
+router.get('/user', function(req, res, next) {
+    res.render('user-profile', { layout: 'dashboard-layout' });
+});
+/* GET User Profile. */
+router.get('/notifications', function(req, res, next) {
+    res.render('notifications', { layout: 'dashboard-layout' });
 });
 
+/* GET User Profile. */
+router.get('/tables', function(req, res, next) {
+    res.render('tables', { layout: 'dashboard-layout' });
+});
+/* GET User Profile. */
+router.get('/upgrade', function(req, res, next) {
+    res.render('upgrade', { layout: 'dashboard-layout' });
+});
 module.exports = router;
